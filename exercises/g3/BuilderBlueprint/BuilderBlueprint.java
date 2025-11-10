61225cca6ddef164d22ed6c45c4e65bcd862b540
Toft <toft.dah@gmail.com>
2025-11-10 20:58:18 +0200

    fix: remove unnecessary main method from BuilderBlueprint

--- exercises/g3/BuilderBlueprint/BuilderBlueprint.java
+++ exercises/g3/BuilderBlueprint/BuilderBlueprint.java
@@ -94,14 +94,4 @@ class RegexDirector {
         builder.buildAnyCharacter();
         return builder.getResult();
     }
-
-        public static void main(String[] args) {
-        RegexDirector director = new RegexDirector();
-        RegexBuilder builder = new ConcreteRegexBuilder();
-
-        director.setBuilder(builder);
-        Regex regex = director.construct();
-
-        System.out.println(regex);
-    }
 }
